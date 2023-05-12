3578d98558a70bfe702ec0c7ee74af2cd40fddd0
Sarkar <parthsarkar125@gmail.com>
2023-05-12 19:46:22 +0530

    first commit

--- app.js
+++ app.js
@@ -91,7 +91,11 @@ app.get("/vamshi",function(req,res){
     res.render("about");
 })
 
-app.listen(3000,function(req,res){
-    console.log("server is running on port 3000");
+let port =process.env.PORT;
+if(port==null || port ==""){
+    port=3000;
+}
+app.listen(port,function(){
+    console.log("server has started successfully");
 });
 
